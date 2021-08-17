1463b25381cf70e2c9193cdcd8f5c625ca49a2f7
al-ru <russkihalex@yandex.ru>
2021-08-17 14:31:43 +0500

    commit

--- App.js
+++ App.js
@@ -1,14 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native'
+import * as Facebook from 'expo-facebook'
+import Expo from 'expo'
+import { Settings } from 'react-native-fbsdk-next'
 
 export default function App() {
+  async function logIn() {
+    try {
+      await Facebook.initializeAsync({
+        appId: '851387698833191',
+      })
+      const {
+        type,
+        token,
+        expirationDate,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile', 'email'],
+      })
+      console.log('type ', type)
+      console.log('token ', token)
+      // console.log('permissions ', permissions)
+
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const uri = `https://graph.facebook.com/me?access_token=${token}`
+        console.log('uri ', uri)
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}`
+        )
+        const name = await response.json()
+        console.log(`response name , ${name.name}`)
+        alert(`Logged in! Hi ${name.name}`)
+        // Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`)
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      console.log('message ', message)
+      alert(`Facebook Login Error: ${message}`)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={() => logIn()}>
+        <View
+          style={{
+            width: '50%',
+            borderRadius: 4,
+            padding: 24,
+            backgroundColor: '#3b5998',
+            marginTop: 10,
+          }}
+        >
+          <Text>Login Facebook</Text>
+        </View>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +72,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
